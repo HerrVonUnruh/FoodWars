e9313656e09fd2f8a2e6b1469513f5e848461bdc
nilsh <nils.henningsen@freenet.de>
2023-10-10 19:23:34 +0200

    ++Camera moves around Table, CameraControl, C++

--- Source/FoodWars/Private/CameraControl.cpp
+++ Source/FoodWars/Private/CameraControl.cpp
@@ -15,13 +15,33 @@ ACameraControl::ACameraControl()
 void ACameraControl::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void ACameraControl::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	changePlayerView();
 }
 
+void ACameraControl::changePlayerView()
+{
+	FVector currentLocation = GetActorLocation();
+	if(FVector::Distance(currentLocation, playerPositions[posIndex]) < 10.0f)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Habe Punkt erreicht! CameraControl: %s"), *FString::Printf(TEXT("Wert: %d"), 42));
+
+	 if(posIndex < 3)
+	 {
+		 posIndex++;
+	 } else 
+	 {
+		 posIndex = 0; 
+	 }
+	} 
+	FVector direction = playerPositions[posIndex] - currentLocation;
+	FVector dirNormalized = direction.GetSafeNormal();
+	currentLocation += dirNormalized * moveSpeed;
+	SetActorLocation(currentLocation);
+; 
+}
