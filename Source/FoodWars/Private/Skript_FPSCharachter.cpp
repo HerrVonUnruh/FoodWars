05ab4896bee1cfdcf4e2fb46ab72c873c6c6ee80
nilsh <nils.henningsen@freenet.de>
2023-10-26 21:21:42 +0200

    ++Fireing mechanics, Skript_FPSCharachter & BP_FPSCharachter, C++ & BP

--- Source/FoodWars/Private/Skript_FPSCharachter.cpp
+++ Source/FoodWars/Private/Skript_FPSCharachter.cpp
@@ -28,9 +28,17 @@ void ASkript_FPSCharachter::Tick(float DeltaTime)
 // Called to bind functionality to input
 void ASkript_FPSCharachter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
+
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//movement input
 	PlayerInputComponent->BindAxis("InputAxis_Forward", this, &ASkript_FPSCharachter::moveForward);
 	PlayerInputComponent->BindAxis("InputAxis_Right", this, &ASkript_FPSCharachter::moveRight);
+	PlayerInputComponent->BindAction("InputAction_Jump", IE_Pressed, this, &ASkript_FPSCharachter::jump);
+	PlayerInputComponent->BindAction("InputAction_Jump", IE_Released, this, &ASkript_FPSCharachter::stopJumping);
+
+
+	//mouse input
 	PlayerInputComponent->BindAxis("InputAxis_MouseLookUp", this, &ASkript_FPSCharachter::rotateCameraUp);
 	PlayerInputComponent->BindAxis("InputAxis_MouseLookRight", this, &ASkript_FPSCharachter::rotateRight);
 }
@@ -80,7 +88,63 @@ void ASkript_FPSCharachter::rotateRight(float value)
 	}
 }
 
+
+void ASkript_FPSCharachter::jump()
+{
+	if (!isJumping)
+	{
+		moveVector = GetActorLocation();
+		isJumping = true;
+		FVector jumpVec = GetActorUpVector() * jumpForce;
+		moveVector += jumpVec;
+		SetActorLocation(moveVector);
+	}
+}
+
+void ASkript_FPSCharachter::stopJumping()
+{
+	isJumping = false; 
+}
 void ASkript_FPSCharachter::setCamera(UCameraComponent* cam)
 {
 	camera = cam; 
+}
+
+void ASkript_FPSCharachter::shoot(TSubclassOf<AActor> bulletClass, FVector barrel, bool debugBool, FRotator barrelRotation)
+{
+	bullet = GetWorld()->SpawnActor<AActor>(bulletClass, barrel, barrelRotation);
+	UStaticMeshComponent* BulletSphereComponent = nullptr;
+	TArray<UStaticMeshComponent*> MeshComponents;
+	bullet->GetComponents<UStaticMeshComponent>(MeshComponents);
+
+	if (MeshComponents.Num() > 0)
+	{
+		BulletSphereComponent = MeshComponents[0];
+	}
+
+
+	if (BulletSphereComponent)
+	{
+		FVector ImpulseDirection = BulletSphereComponent->GetUpVector();
+		float ImpulseStrength = -1000.0f;
+		UE_LOG(LogTemp, Warning, TEXT("Impulse Direction: %s"), *ImpulseDirection.ToString());
+		UE_LOG(LogTemp, Warning, TEXT("Impulse Strength: %f"), ImpulseStrength);
+		BulletSphereComponent->AddImpulse(ImpulseDirection * ImpulseStrength);
+	} else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("NO SPHERE!!!!! WARNING ACHTUNG!!!!!NO SPHERE %s"));
+	
+	}
+}
+
+void ASkript_FPSCharachter::moveBullet(float timeSpan,float deltaTime)
+{
+  while(timeSpan > 0)
+  {
+
+	  FVector bulletPos = bullet->GetActorLocation(); 
+	  bulletPos += bullet->GetActorForwardVector() * 10.0F*deltaTime; 
+	  bullet->SetActorLocation(bulletPos); 
+	  timeSpan--; 
+  } 
 }
