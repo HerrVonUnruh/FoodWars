d4999f51f2e6161473b26f749bd399c702cf2020
nilsh <nils.henningsen@freenet.de>
2024-01-16 20:35:20 +0100

    ++Added Prototype for Camera Cycle-Movement, CameraTest.cpp & BP_CameraTest, Cpp & BP

--- Source/FoodWars/Private/CameraTest.cpp
+++ Source/FoodWars/Private/CameraTest.cpp
@@ -3,6 +3,7 @@
 
 #include "CameraTest.h"
 
+
 // Sets default values
 ACameraTest::ACameraTest()
 {
@@ -23,21 +24,45 @@ void ACameraTest::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	deltaTime = DeltaTime;
-
+	RotateCamToCenter(DeltaTime); 
 }
 
 // Called to bind functionality to input
 void ACameraTest::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	PlayerInputComponent->BindAxis("InputAxis_Right", this,&ACameraTest::MoveCamera);
 }
 
-void::ACameraTest::MoveCamera()
+void::ACameraTest::MoveCamera(float Value)
 {
-	float angle = deltaTime * rotationSpeed;
 
+
+	if (FMath::Abs(Value) > 0)
+	{
+		// Multiplizieren Sie mit DeltaTime, um die Rotationsgeschwindigkeit zu skalieren
+		float RotationSpeedScaled = rotationSpeed * deltaTime;
+
+		// Inkrementieren Sie den Rotationswinkel basierend auf der Skalierung und Benutzereingabe
+		CurrentRotationAngle += RotationSpeedScaled * Value;
+
+		// Berechnen Sie die neue Position der Kamera auf der Kreisbahn
+		FVector NewCameraPosition = Center + FVector(moveCycleRadius * FMath::Cos(CurrentRotationAngle), moveCycleRadius * FMath::Sin(CurrentRotationAngle), 1000);
+
+		// Setzen Sie die neue Position der Kamera
+		SetActorLocation(NewCameraPosition);
+	}
 	
+}	
 
+void::ACameraTest::RotateCamToCenter(float DeltaTime)
+{
+	FRotator targetRotation = (Center - GetActorLocation()).Rotation();
+	FRotator NewRot = FMath::RInterpTo(GetActorRotation(), targetRotation, DeltaTime, 1);
+	SetActorRotation(NewRot);
 }
 
+float::ACameraTest::GetDeltaTime()
+{
+	return deltaTime; 
+}
